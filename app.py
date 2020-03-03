9d6de41bbf828dace6323e2f9a6d20803b692e87
manh102 <manhtt102@gmail.com>
2020-03-03 16:44:24 +0700

    add homepage html

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from security import authenticate, identity
 from resources.user import UserRegister
 from resources.item import Item, ItemList
 from resources.store import Store, StoreList
+from resources.homepage import Homepage
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
@@ -22,6 +23,7 @@ def create_tables():
 
 jwt = JWT(app, authenticate, identity)  # /auth
 
+api.add_resource(Homepage, '/')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 api.add_resource(Item, '/item/<string:name>')
